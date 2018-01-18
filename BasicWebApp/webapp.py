23b593f8376cf1650901a8e098da2ecfbfaab8db
Lowens1996 <32193115+Lowens1996@users.noreply.github.com>
2018-01-18 21:04:55 +0000

    Add files via upload

--- BasicWebApp/webapp.py
+++ BasicWebApp/webapp.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, session, jsonify
 import pymongo
 import os
+import paypalrestsdk
+
+paypalrestsdk.configure({
+  "mode": "sandbox",  # sandbox or live
+  "client_id": "ASNdXIYOglgkncwMqPFadYDjhKmlET-iFnb8JY_EuBol59DIZGcX0vYrRl3lRhw2nHKAXE5ACBMIGlP9",  # merchant account
+  "client_secret": "EJ6ZHrvWB5mU_Te6RSNwIaFra6PNUzk63OY-0jmaUazUKuLlZJu1IGu5dhinmbNdWXT4TKq4-4VdV1mj"})
 
 app = Flask(__name__)
 client = pymongo.MongoClient()
@@ -67,23 +73,67 @@ def draft_page():
     return render_template('Draft.html')
 
 
-@app.route('/HandleOrder', methods=['POST'])
+@app.route('/PreviewOrder', methods=['POST'])
 def handle_order():
     global totalprice
     if 'Submit' in request.form:
         return render_template('Orderlist.html',
-                               list=orderlist)
+                               list=orderlist, totalprice=totalprice)
     else:
         for k, v in request.form.items():  # k = product name v = the quantity selected
             v = int(v)
             if v > 0:
                 orderlist.append(k)  # add product name to the list
-                orderlist.append(v)  # add quantity selected to the list
-                price = specials.find_one({'product_name': k})
+                orderlist.append("X" + str(v))  # add quantity selected to the list
+                for product in specials.find():
+                    if product['product_name'] == k:
+                        price = product['product_price']
+                        break
                 itemprice = price * v
                 orderlist.append(itemprice)  # price x quantity
-                totalprice = totalprice + float(v)
+                totalprice = totalprice + float(itemprice)
         return render_template('OrderPage.html')
+
+
+@app.route('/payment', methods=['POST'])
+def payment():
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "item",
+                    "sku": "item",
+                    "price": "100.00",
+                    "currency": "GBP",
+                    "quantity": 1}]},
+            "amount": {
+                "total": "100.00",
+                "currency": "GBP"},
+            "description": "This is the payment transaction description."}]})
+
+    if payment.create():
+        print('payment success')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID': payment.id})
+
+
+@app.route('/executePayment', methods=['POST'])
+def execute():
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id': request.form['payerID']}):
+        print('payment executed')
+    else:
+        print(payment.error)
 
 
 if __name__ == '__main__':
